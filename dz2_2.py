c8612a7fac6ddbc34d128ff3daff7a3bd16297ed
ROANVL <98936903+ROANVL@users.noreply.github.com>
2022-02-17 21:50:12 +0300

    Update dz2_2.py

--- dz2_2.py
+++ dz2_2.py
@@ -2,22 +2,22 @@ a = input('Введите значение переменной a:')
 b = input('Введите значение переменной b:')
 c = input('Введите значение переменной c:')
 
-chislitel = (float(a)**2) + (float(b)**2)
-znamenatel = (3*float(b)) - 4
-chislitel2 = 4*(float(c)**5)
-znamenatel2 = 6
+numerator = (float(a)**2) + (float(b)**2)
+denominator = (3*float(b)) - 4
+numerator2 = 4*(float(c)**5)
+denominator = 6
 
-delimoe = chislitel / znamenatel
-delitel = chislitel2 / znamenatel2
+divisible = numerator / denominatorl
+divider = numerator2 / denominator2
 
-nepolnoe_chastnoe = delimoe // delitel
-ostatok = delimoe % delitel
+nepolnoe_chastnoe = divisible // divider
+ostatok = delimoe % divider
 
-print(float(round(chislitel, 2)))
-print(float(round(znamenatel, 2)))
-print(float(round(chislitel2, 2)))
-print(float(round(znamenatel2, 2)))
-print(float(round(delimoe, 2)))
-print(float(round(delitel, 2)) ,'\n')
+print(float(round(numerator, 2)))
+print(float(round(denominatorl, 2)))
+print(float(round(numerator2, 2)))
+print(float(round(denominatorl2, 2)))
+print(float(round(divisible, 2)))
+print(float(round(divider, 2)) ,'\n')
 print('Целая часть', round(nepolnoe_chastnoe, 2))
 print('Остаток от деления', round(ostatok, 2))
